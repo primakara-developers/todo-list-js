870b640fd7f5bd72632d45c28b0abcc0ad9912fc
Dipadana Putu <putudipadana@gmail.com>
2021-04-27 15:45:16 +0800

    Update save and delete function

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
 let todoList = [];
-let inputText = 
+let inputText = document.getElementById("input_field");
+let container = document.getElementById("todo_list_container");
+
+function initDataToContainer() {
+  container.innerHTML = "";
+  todoList.forEach((value, i) => {
+    container.innerHTML += `
+      <p id="${value + i}" onclick="remove('${value}')">
+        ${value}
+      </p>
+    `;
+  });
+}
+
+function save() {
+  todoList.push(inputText.value);
+  inputText.value = "";
+  initDataToContainer();
+}
+
+function remove(listValue) {
+  let tempTodoList = [];
+  todoList.forEach((value) => {
+    if (listValue !== value) {
+      tempTodoList.push(value);
+    }
+  });
+  todoList = tempTodoList;
+  initDataToContainer();
+}
+
+document.getElementById("save_button").addEventListener("click", () => {
+  save();
+});
