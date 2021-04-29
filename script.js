2e6be99c9a17ef4205146d0ba08630040ef67aee
Dipadana Putu <putudipadana@gmail.com>
2021-04-29 18:14:58 +0800

    Update variable name and add several comment for explain the logic

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
+// Init variable
 let todoList = [];
-let inputText = document.getElementById("input_field");
-let container = document.getElementById("todo_list_container");
+let inputTodoList = document.getElementById("input_field");
+let todoListContainer = document.getElementById("todo_list_container");
 
-function initDataToContainer() {
-  container.innerHTML = "";
+function getData() {
+  // Empty the container first
+  todoListContainer.innerHTML = "";
+
+  // Then fill the container with value
   todoList.forEach((value, i) => {
-    container.innerHTML += `
-      <p id="${value + i}" onclick="remove('${value}')">
+    todoListContainer.innerHTML += `
+      <p id="${value + i}" onclick="removeData('${value}')">
         ${value}
       </p>
     `;
   });
 }
 
-function save() {
-  todoList.push(inputText.value);
-  inputText.value = "";
-  initDataToContainer();
+function createData() {
+  // Asign the todo list value to  todo list array
+  todoList.push(inputTodoList.value);
+
+  // After that, clear the input field
+  // and update the data in container
+  inputTodoList.value = "";
+  getData();
 }
 
-function remove(listValue) {
+function removeData(listValue) {
+  // Array for contain temporary filtered data
   let tempTodoList = [];
+
+  // Filter the data & assign the temporary array to todo list array
   todoList.forEach((value) => {
     if (listValue !== value) {
       tempTodoList.push(value);
     }
   });
   todoList = tempTodoList;
-  initDataToContainer();
+
+  // After that update the data in container
+  getData();
 }
 
+// This event for save button and trigger save function
 document.getElementById("save_button").addEventListener("click", () => {
-  save();
+  createData();
 });
